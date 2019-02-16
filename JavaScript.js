0407d7f2859ae9be69b4aca68fab4bafa126f759
Alexis Williams <47547973+AlexisWilliams1030@users.noreply.github.com>
2019-02-15 23:24:17 -0500

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,51 @@
+﻿
+//Setting the user score to 0
+
+var score = 0;
+
+//Question 1
+
+var quest1 = prompt("What is 2 + 2?");
+
+if (quest1 === "4") {
+    alert("You are correct!");
+    score = score + 1;
+}
+
+else {
+    alert("Sorry that is not correct");
+}
+var quest2 = prompt("What is 5 * 5?");
+
+//Question 2
+
+if (quest2 === "25") {
+    alert("You are correct!");
+    score = score + 1;
+}
+
+else {
+    alert("Sorry that is not correct");
+}
+var quest3 = prompt("What is 1 + 1?");
+
+//Question 3
+
+if (quest3 === "2") {
+    alert("You are correct!");
+    score = score + 1;
+}
+
+else {
+    alert("Sorry that is not correct");
+}
+
+//Checking for passing score
+
+if (score === 2 || score === 3) {
+    document.write("You have passed the quiz");
+}
+
+else {
+    document.write("sorry you did not pass");
+}
